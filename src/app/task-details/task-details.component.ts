33414741e45161642c372792e9717f20d91669dc
Lucas Vargas <lukeamfernandes@gmail.com>
2024-09-23 16:41:13 -0300

    funcionaliadades dos componentes

--- src/app/task-details/task-details.component.ts
+++ src/app/task-details/task-details.component.ts
@@ -1,12 +1,58 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute,Router } from '@angular/router';
+import { Task } from '../models/task';
+import { TaskService } from '../services/task.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-details',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './task-details.component.html',
   styleUrl: './task-details.component.css'
 })
 export class TaskDetailsComponent {
 
+  task?: Task;
+
+  constructor(private router: Router,private route: ActivatedRoute, private taskService: TaskService) { }
+
+  ngOnInit() {
+
+    let id = this.route.snapshot.paramMap.get('id');
+
+    if(id === null) {
+      this.navigateBack();
+    }    
+    else {
+      /*"+" representa a forma da passagem de id enquanto string, para número.*/
+      this.task = this.taskService.getById(+id);
+
+      if(this.task === undefined) {
+        this.navigateBack();
+      }
+    }
+  }
+
+  save() {
+    this.taskService.updateTasks();
+
+    this.navigateBack();
+  }
+
+  cancel() {
+    this.navigateBack();
+  }
+
+  private navigateBack() {
+    /*navigate: é usado para programaticamente navegar entre diferentes 
+    rotas dentro de uma aplicação Angular. Ele faz parte do serviço 
+    Router e permite que o usuário se redirecione para uma nova rota, 
+    especificando o caminho desejado e, opcionalmente, parâmetros 
+    adicionais.*/
+
+    /*relativeTo indica que a navegação deve ser relativa à rota atual 
+    (this.route), permitindo uma navegação mais flexível e dinâmica.*/
+    this.router.navigate(['/taskList'], {relativeTo: this.route}); 
+  }
 }
--- src/app/task-filter/task-filter.component.ts
+++ src/app/task-filter/task-filter.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-task-filter',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './task-filter.component.html',
   styleUrl: './task-filter.component.css'
 })
 export class TaskFilterComponent {
+    
+    filter: string = '';
 
+    /*EventEmitter: emite eventos de qualquer parte de uma aplicação e 
+    escuta esses eventos para tomar ações específicas.*/
+    @Output() taskFilter = new EventEmitter<string>();
+
+    filterTask() {
+      this.taskFilter.emit(this.filter);
+    }
 }
